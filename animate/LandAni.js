2175c3ff785f81b58d477daa213a858246711943
aCodeReader <jacobs0408@yahoo.com>
2019-04-08 18:54:00 -0500

    stash

---
+++ animate/LandAni.js
@@ -0,0 +1,32 @@
+var popup = document.getElementById('popup');
+var popup_window = document.getElementById('popup_window');
+popup_window.style.display = 'none';
+
+popup.onclick = function() { 
+  if(popup_window.style.display === 'none') {
+    popup_window.style.display = 'block';
+    var window_height = popup_window.offsetHeight + 35;
+    popup_window.style.top = '-' + window_height + 'px';
+  } else {
+    popup_window.style.display = 'none';
+  }
+}
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+let spans = document.querySelectorAll('span');
+spans.forEach((span, idx) => {
+    span.addEventListener('click', (e) => {
+    e.target.classList.add('active');
+      });
+      span.addEventListener('animationend', (e) => {
+          e.target.classList.remove('active');
+      });
+      
+      // Initial animation
+      setTimeout(() => {
+          span.classList.add('active');
+      }, 750 * (idx+1))
+  });
